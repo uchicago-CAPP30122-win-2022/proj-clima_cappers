d4f714d89e4a01b6ec1f87bf9b9251d5663e5105
Kaveri Chhikara <kaveri@cs.uchicago.edu>
2022-03-06 18:41:09 -0600

    Updates- requirements.txt and climate paramters

--- climate_parameters_api.py
+++ climate_parameters_api.py
@@ -1,15 +1,4 @@
-from argparse import _CountAction
-from inspect import Parameter
-from matplotlib.style import library
-from sodapy import Socrata
 import requests
-
-
-# def _decode_libray_data(dct):
-#     #First check that all required attributes are inside the dictionary 
-#     if all (key in dct for key in ["name_","location","address"]):
-#         return Library(dct["name_"],(dct["location"]['latitude'],dct["location"]['longitude']) ,dct["address"])
-#     return dct
 
 # https://api.worldbank.org/v2/country/indicator/SP.POP.TOTL?date=2000:2001
 
@@ -19,12 +8,10 @@ ALL_PARAMETERS = ["EN.ATM.CO2E.PC", "AG.LND.FRST.ZS", "EN.ATM.PM25.MC.M3",
 class DataCollector: 
 
     def __init__(self):
-        # Unauthenticated client only works with public data sets. Note 'None'
-        # in place of application token, and no username or password:
-        self.country_client = "https://api.worldbank.org/v2/country?format=json&page=%s"
-        
-        #second client for country
-        self.main_client = "https://api.worldbank.org/v2/{country}/indicator/{parameter}?date=1995:2020"
+        self.country_client = "https://api.worldbank.org/v2/country?format=json"\
+                               "&page=%s"
+        self.main_client = "https://api.worldbank.org/v2/country/{country}/"\
+                           "indicator/{parameter}?date=1995:2021"
 
 
     def find_countries(self):
@@ -34,30 +21,21 @@ class DataCollector:
             json_data = results.json()
             dict_countries = json_data[1]
             for d in dict_countries:
-                # check1 = d.get("iso2Code", "NA")
-                # check2 = d.get("iso2code", "NA")    
-                # # if not d.get("iso2Code", "NA") and d.get("iso2code", "NA"):
-                # if check1 != "NA" and check2 != "NA":
-                #     print(d["id"])
                 countries.append(d["id"])
         return countries
+
 
     def get_data(self):
         all_countries = self.find_countries()
         for country in all_countries:
             for parameter in ALL_PARAMETERS:
-                results = requests.get(self.main_client.format(country, parameter))
-                
+                results = requests.get(self.main_client.format(country = country, 
+                                                            parameter = parameter))
+                json_data = results.json()
+                ## create a dataframe for each country, year, indicator
+                ## get the next indicator for this countryy and join it with this dataset
+                ## push this dataset where each row is a (country, year, indicator1, i2) tuple
+                # to the databse
 
-
-
-    # def find_indicators(self, limit):
-    #     libraries = []
-    #     results = self.client.get("x8fc-8rcq",limit=limit)
-    #     for lib_dict in results: 
-    #         library = _decode_libray_data(lib_dict)
-    #         libraries.append(library)
-
-    #     return libraries
 
 
