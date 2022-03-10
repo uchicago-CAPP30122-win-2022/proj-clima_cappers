fdd9b22dc6de7400e317bb2fe0cb8ae05dcde551
Kaveri Chhikara <kaveri@cs.uchicago.edu>
2022-03-09 21:49:52 -0600

    Updated Indicator list, added Mean surface temp

--- climate_parameters_api.py
+++ climate_parameters_api.py
@@ -9,14 +9,20 @@ connection = sqlite3.connect("indicators.sqlite3")
 c = connection.cursor()
 
 # parameterized sql query for db insert
-sql_insert = "INSERT INTO climate_indicators VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+sql_insert = '''INSERT INTO climate_indicators VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                ? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''' 
+
 
 # to be removed in final draft
 sql_distinct = "SELECT DISTINCT(iso_code) FROM climate_indicators"
 distinct_codes = c.execute(sql_distinct).fetchall()
 
-ALL_PARAMETERS = ["EN.ATM.CO2E.PC", "AG.LND.FRST.ZS", "EN.ATM.PM25.MC.M3",
-              "EN.ATM.SF6G.KT.CE", "EN.ATM.GHGT.KT.CE" ]
+ALL_PARAMETERS = ["NY.GDP.PCAP.CD", "SI.POV.GINI", "EN.ATM.CO2E.KT", "EN.ATM.CO2E.PC", 
+                  "AG.LND.FRST.ZS","SP.POP.TOTL", "EG.ELC.HYRO.ZS", "EG.ELC.NGAS.ZS", 
+                  "EG.ELC.NUCL.ZS","EG.ELC.PETR.ZS", "EG.ELC.COAL.ZS", "EG.ELC.FOSL.ZS", 
+                  "EG.ELC.RNWX.ZS","EN.ATM.PM25.MC.M3", "EN.ATM.SF6G.KT.CE", 
+                  "EN.ATM.GHGT.KT.CE", "CC.GHG.GRPE", "SI.POV.GINI", "CC.GHG.PECA", 
+                  "NE.EXP.GNFS.KD", "NE.IMP.GNFS.KD"]
 
 class DataCollector: 
 
@@ -42,7 +48,7 @@ class DataCollector:
         all_countries = self.find_countries()
         print("beginning indicator data extraction")
         for name, code in all_countries:
-            if (code,) in distinct_codes:
+            if (code,) in distinct_codes and code != "SSF": # "TCA" "ROU": #"MEX" "EST" "FSM" "ITA": #and code != "BRA" and code != "CAN" and code != "ERI": # switzerland
                 continue
             print("currently in country", name)
             d = {"Country": [name]*26, "Country_code": [code]*26, 
@@ -70,22 +76,12 @@ class DataCollector:
             connection.commit()
         connection.close()
 
+        
+
 
 if __name__ == "__main__":
     obj = DataCollector()
     obj.get_data()
-
-
-
-                
-
-                # data = pd.DataFrame(list(results.json()[1].items()),
-                #                          columns = ['column1','column2'])
-
-                ## create a dataframe for each country, year, indicator
-                ## get the next indicator for this countryy and join it with this dataset
-                ## push this dataset where each row is a (country, year, indicator1, i2) tuple
-                # to the databse
 
 
 
--- create-db.sql
+++ create-db.sql
@@ -12,13 +12,38 @@ CREATE TABLE econ_indicators
 .separator ","
 .import econ_parameters_out.csv econ_indicators
 
+CREATE TABLE mean_temp_indicator
+(country varchar(15),
+  iso_code varchar(5),
+  year integer,
+  mean_temp float,
+  PRIMARY KEY (iso_code, year));
+
+.separator ","
+.import mean_st_output.csv mean_temp_indicator
+
 CREATE TABLE climate_indicators
   (country varchar(15),
    iso_code varchar(5),
    year integer,
-   co2_emissions float,
+   gdp_capita float,
+   gini_index float,
+   co2_emissions_kt float,
+   co2_emissions_capita float,
    forest_area float,
+   population_total float,
+   electricity_pro_hydro float,
+   electricity_pro_natural_gas float,
+   electricity_pro_nuclear float,
+   electricity_pro_oil float,
+   electricity_pro_coal float,
+   electricity_pro_fossils float,
+   electricity_pro_renewable float,
    pm_25 float,
    sf6_emissions float,
-   greenhouse_total float,
+   ghg_total float,
+   ghg_growth float,
+   ghg_capita float,
+   exports_gns float,
+   imports_gns floats,
    PRIMARY KEY (iso_code, year));
