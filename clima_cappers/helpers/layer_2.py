aac18b7f6f197b1ee6a4d8a477b4303e7c282718
Dhruv Sinha <dhruvs@linux7.cs.uchicago.edu>
2022-03-16 13:04:33 -0500

    layer_1

--- clima_cappers/helpers/layer_2.py
+++ clima_cappers/helpers/layer_2.py
@@ -5,6 +5,24 @@ import sqlite3
 connection = sqlite3.connect("./data/indicators.sqlite3", check_same_thread=False)
 
 def extract_agg_data(indicator, table, year_input):
+    """
+    This function aggegates the selected indicator for a range of years
+
+    Arguments
+    --------------
+    indicator- str
+              the name of the indicator that the user selects on the page
+    table- str
+            the name of the table that we will be querying the data from
+    year_input- list
+            the list with start and end year
+
+    Returns
+    -------------
+    df_agg- pandas dataframe
+            pandas dataframe that contains the indicator aggregated for multiple
+            years
+    """
     print("year input type is", type(year_input))
     indicator_agg_simpavg= ["mean_surface_temp",
                             "co2_emissions_kt",
@@ -35,16 +53,69 @@ def extract_agg_data(indicator, table, year_input):
     return df_agg
 
 def aggregate_simple_average(df, indicator):
+    """
+    This function takes the simple average of the indicator
+
+    Arguments
+    ---------------
+    df: pandas dataframe
+        the dataframe that contains the value of an indicator for the selected
+        years
+    indicator- str
+        the name of the indicator
+
+    Returns
+    --------------
+    df_new- pandas dataframe
+        the dataframe that contains the indicator value averaged for each country
+    """
     df_new= df.groupby(['country','iso_code'], as_index=False).mean()
     df_new.rename(columns = {None: indicator}, inplace = True)
     return df_new
 
 def w_avg_1(df_weigh, values, weights):
+    """
+    This function calculate 
+
+    Arguments
+    ---------------
+    df_weigh: pandas dataframe
+        the dataframe that contains the value of an indicator for the selected
+        years for a country
+    values- str
+        the name of the indicator for which we want take the average
+    weights- str
+        the name of the indicator by which we want to weigh the value
+
+    Returns
+    --------------
+    float
+        the value for each country
+    """
     d = df_weigh[values]
     w = df_weigh[weights]
     return (d * w).sum() / w.sum()
 
 def aggregate_weight_1(df_get, indicator):
-    df_new= df_get.groupby(['iso_code', 'country'], as_index=False).apply(w_avg_1, indicator, 'population')
+    """
+    This function calculates the weighted average for each country.
+
+    Arguments
+    ---------------
+    df_weigh: pandas dataframe
+        the dataframe that contains the value of an indicator for the selected
+        years
+    values- str
+        the name of the indicator for which we want take the average
+    weights- str
+        the name of the indicator by which we want to weigh the value
+
+    Returns
+    --------------
+    df_new- pandas datafram
+        the dataframe that contains the indicator value averaged for each country
+    """
+    df_new= df_get.groupby(['iso_code', 'country'], as_index=False).apply(w_avg_1, indicator, 
+                                                                            'population')
     df_new.rename(columns = {None: indicator}, inplace = True)
     return df_new
