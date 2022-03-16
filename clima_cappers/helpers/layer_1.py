7ebf9f735f574edfb0d2916f025d0def3dd2077e
Vishal Vincent Joseph <vjoseph@linux4.cs.uchicago.edu>
2022-03-16 14:37:26 -0500

    cleaned up code and added docstrings - vvj

--- clima_cappers/helpers/layer_1.py
+++ clima_cappers/helpers/layer_1.py
@@ -6,12 +6,50 @@ connection = sqlite3.connect("./data/indicators.sqlite3", check_same_thread=Fals
 
 
 def extract_map_data(indicator, table, year):
+    """
+    This function returns the selected indicator for a given year.
+
+    Arguments
+    --------------
+    indicator- str
+              the name of the indicator that the user selects on the page
+    table- str
+            the name of the table that we will be querying the data from
+    year- str
+            the selected year
+
+    Returns
+    -------------
+    df- pandas dataframe
+            pandas dataframe that contains the indicator for the
+            given year
+    """
     query = ''' SELECT country, iso_code, {} FROM {} WHERE year = {} '''. \
                 format(indicator, table, year)
     df = pd.read_sql_query(query, connection)
     return df
 
+
 def extract_bar_data(indicator, table, year):
+    """
+    This function returns the top 5 countries for a selected
+    indicator for a given year.
+
+    Arguments
+    --------------
+    indicator- str
+              the name of the indicator that the user selects on the page
+    table- str
+            the name of the table that we will be querying the data from
+    year- str
+            the selected year
+
+    Returns
+    -------------
+    df- pandas dataframe
+            pandas dataframe that contains the top 5 countries for a 
+            selected indicator for the given year
+    """
     query = ''' SELECT country, iso_code, {} FROM {} 
                 WHERE year = {} AND TRIM({}) <> '' AND iso_code <> 'WLD'
                 ORDER BY {} DESC LIMIT 5 '''. \
@@ -19,7 +57,25 @@ def extract_bar_data(indicator, table, year):
     df = pd.read_sql_query(query, connection)
     return df
 
+
 def trend_df(indicator_lst, country):
+    """
+    This function returns the indicators selected for comparison
+    for a specific country chosen by the user.
+
+    Arguments
+    --------------
+    indicator_lst- list
+              the list of indicator chosen for comparison by the user
+    country- str
+            the selected country
+
+    Returns
+    -------------
+    df- pandas dataframe
+            pandas dataframe that contains the indicators for the
+            selected country 
+    """
     params = [country]
     if isinstance(indicator_lst, list):
         format_lst = ','.join(indicator_lst)
